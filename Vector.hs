6c354553a0e7262e6fb941cf17b3c533d54b3f0c
Finlay Thompson <finlay@dragonfly.co.nz>
2012-08-15 00:20:11 +1200

    Trying to get zero to work! not quite there yet

--- Vector.hs
+++ Vector.hs
@@ -1,28 +1,34 @@
-{- LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 {- LANGUAGE MultiParamTypeClasses #-}
-{- LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 {- LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE DataKinds #-}
 module Vector where
 
-import Data.Array.Repa
+import GHC.TypeLits
 
-class AdditiveGroup v where 
-    data Elem v :: *
-    dimension :: v -> Int
-    zero   :: Elem v
-    minus  :: Elem v -> Elem v
-    plus   :: Elem v -> Elem v -> Elem v
+--import Data.Array.Repa
+import qualified Data.Vector.Unboxed as V
+
+class Additive v where 
+    zero   :: v
+    minus  :: v -> v
+    plus   :: v -> v -> v
+
 
 -- Need to encode the dimension at both the type and data level.
 -- Dimension is used to create the zero element.
 -- Also want a unique identity, to statically ensure that linear
 --   maps are applied correctly.
-data Nat = Zero | Succ Nat
+data LinearSpan ( d :: Nat ) elm  
+data family Elem a :: *
+data instance Elem (LinearSpan d e) = EL (V.Vector e)
 
-data Rag r e
-instance (Source r e) => AdditiveGroup (Rag r e) where
-    newtype Elem (Rag d) = R (Shape r DIM1 e)
-    dimension (Rag d) = d
-    zero    = R $ fromFunction (Z :. (4 :: Int)) (const 0.0)
-    minus v =  
-    
+instance (V.Unbox e, Num e) => Additive (Elem (LinearSpan d e)) where
+    zero   = let mkZero :: Sing d -> V.Vector e
+                 mkZero d = V.replicate (fromSing d) 0
+		     in  EL (withSing mkZero)
+    minus  = undefined
+    plus   = undefined
---
+++ quaternions.hs
@@ -0,0 +1,4 @@
+{-# LANGUAGE DataKinds #-}
+import Vector
+
+type H = LinearSpan 4 Double
