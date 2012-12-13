8be816d1bed551dcfafd49dee36230bc0639bd11
Finlay Thompson <finlay@dragonfly.co.nz>
2012-12-14 00:12:44 +1300

    Added basis class and control

--- Extensive.hs
+++ Extensive.hs
@@ -1,4 +1,9 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
 
 module Extensive where
 
@@ -15,37 +20,73 @@ instance Functor V where
     fmap f (V xs) = V $ xs . (flip ((flip id) . f))
 
 
-tensor :: V a -> V a -> V (a, a)
-tensor tx ty =  (join . (fmap t') . t'') (tx, ty)
+tensor :: V a -> V b -> V (Tensor a b)
+tensor tx ty =  (fmap (\(x,y) -> Tensor x y) . join . (fmap t') . t'') (tx, ty)
     where
         t'' (x, y) = fmap (x,) y
         t'  (x, y) = fmap (,y) x
 
-class FiniteSet x where 
-    elements :: [ x ]
-data H = E | I | J | K deriving (Eq, Ord, Show)
-instance FiniteSet H where elements = [ E, I, J, K ]
+data Tensor a b = Tensor a b
 
-instance (FiniteSet x, FiniteSet y) => FiniteSet (x,y) where
-    elements = [ (a,b) | a <- elements, b <- elements ]
+class FiniteSet x where elements :: [ x ]
+instance (FiniteSet x, FiniteSet y) => FiniteSet (Tensor x y) where
+    elements = [ Tensor a b | a <- elements, b <- elements ]
+
+instance (Show x, Show y) => Show (Tensor x y) where
+    show (Tensor x y) = show x ++ " \x2297 " ++ show y
+
+instance (Eq x, Eq y) => Eq (Tensor x y) where
+    Tensor x y == Tensor x' y' = x == x' && y == y'
+
+instance (Ord x, Ord y) => Ord (Tensor x y) where
+    Tensor x y <= Tensor x' y' = x <= x' && y <= y'
+
+instance (Eq a, FiniteSet a) => Eq (V a) where
+    x == y = (coefficients x) == (coefficients y)
+
+instance (Eq a, FiniteSet a, Ord a) => Ord (V a) where
+    compare x y = compare (coefficients x) (coefficients y)
+
+extend :: (Monad m) => (a -> m b) -> m a -> m b
+extend = flip (>>=)
 
 delta :: (Eq x) => x -> x -> R
 delta a b = if a == b then 1 else 0
 
-coef :: (FiniteSet x, Eq x) => V x -> [R]
-coef v = map (unV v . delta ) elements
+-- Basis
+class Basis b x where
+    eta   :: b -> x -> R
+    coef  :: V x -> b -> R
+    coef (V v) = v . eta
 
-e = return E :: V H
-i = return I :: V H
-j = return J :: V H
-k = return K :: V H
+-- canonical basis
+instance (Eq x) => Basis x x where
+    eta = delta
+
+coefficients :: (FiniteSet x, Eq x) => V x -> [(x, R)]
+coefficients (V v) = map (\e -> (e, v (delta e))) elements
+
+showInBasis :: (Show b, Eq b, Basis b x) 
+            => [b] -> V x -> String
+showInBasis bs v =
+        let pairs = map (\e -> (e, coef v e)) bs
+            showPair (b, n) 
+               | n == 1.0    = " + "                  ++ show b
+               | n == -1.0   = " - "                  ++ show b
+               | n > 0       = " + " ++ showN n       ++ show b
+               | otherwise   = " - " ++ showN (abs n) ++ show b
+            showN n = if n == fromInteger (round n) 
+                       then show (round n) 
+                       else show n
+        in  case map showPair . filter (\(_,n) -> n /= 0.0) $ pairs of 
+                  [] -> " 0"
+                  ss -> concat ss
 
 
-
-
-
-
-
+instance (Eq a, FiniteSet a, Show a) => Show (V a) where
+    show = let showInCanonicalBasis :: (Show a, Eq a) => [a] -> V a -> String
+               showInCanonicalBasis = showInBasis 
+           in  showInCanonicalBasis elements
 
 zero :: V a
 zero = V $ \_ -> 0
@@ -55,3 +96,22 @@ minus (V x) = V $ \f -> - (x f)
 
 plus :: V a -> V a -> V a
 plus (V x) (V y) = V $ (\ar -> x ar + y ar)
+
+scale :: R  -> V a -> V a
+scale r (V x) = V $(\ar -> r * (x ar))
+
+-- ALGEBRAS (Associative)
+class Algebra x where
+    unit :: x
+    mul  :: x -> x -> x
+
+-- Working around the terrible Num class here
+instance Algebra (V v) => Num (V v) where
+    (+) v w        = plus v w
+    (-) v w        = plus v (minus w)
+    negate w       = minus w
+    (*) v w        = mul v w
+    abs            = undefined
+    signum         = undefined
+    fromInteger i  = scale (fromInteger i) unit
+
--- quaternions.hs
+++ quaternions.hs
@@ -1,92 +1,92 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeFamilies #-}
-{- LANGUAGE DataKinds #-}
-{- LANGUAGE TypeOperators #-}
-{- LANGUAGE MultiParamTypeClasses #-}
-import Vector
-
+{-# LANGUAGE MultiParamTypeClasses #-}
 import Text.Printf
-import Data.List
+
+import Extensive
+
 
 data H = E | I | J | K deriving (Eq, Ord)
-
-instance Span H where
-    type Scalar H = Double
-    canonical = [E, I, J, K]
-
+instance FiniteSet H where elements = [ E, I, J, K ]
 instance Show H where
     show E = "e"
     show I = "i"
     show J = "j"
     show K = "k"
 
-[e,i,j,k] = elements (canonicalBasis E)
+[e,i,j,k] = map return elements :: [V H]
 
-instance Show (Elem H) where
-    show = showInBasis (canonicalBasis E)
+mu :: V (Tensor H H) -> V H
+mu = extend mu'
+  where
+    mu' :: Tensor H H -> V H
+    mu' (E `Tensor` b) = return b
+    mu' (b `Tensor` E) = return b
+    mu' (I `Tensor` J) = k
+    mu' (J `Tensor` K) = i
+    mu' (K `Tensor` I) = j
+    mu' (J `Tensor` I) = minus k
+    mu' (K `Tensor` J) = minus i
+    mu' (I `Tensor` K) = minus j
+    mu' (I `Tensor` I) = minus e
+    mu' (J `Tensor` J) = minus e
+    mu' (K `Tensor` K) = minus e
 
---mu :: Elem (Tensor H H) -> Elem H
---mu = extend mu'
---  where
---    mu' :: Tensor H H -> Elem H
---    mu' (Tensor E b) = embed b
---    mu' (Tensor b E) = embed b
---    mu' (Tensor I J) = k
---    mu' (Tensor J K) = i
---    mu' (Tensor K I) = j
---    mu' (Tensor J I) = minus k
---    mu' (Tensor K J) = minus i
---    mu' (Tensor I K) = minus j
---    mu' (Tensor I I) = minus e
---    mu' (Tensor J J) = minus e
---    mu' (Tensor K K) = minus e
---
---instance Algebra H where
---    unit    = e
---    mul x y = mu (x `tensor` y)
---
----- Now lets make Tensor H H an algebra
---instance Algebra (Tensor H H) where
---    unit = e `tensor` e
---    mul x y = extend muHH (x `tensor` y)
---            where 
---                muHH  (Tensor (Tensor x y) (Tensor x' y')) 
---                    = ((embed x) * (embed x')) `tensor` ((embed y') * (embed y))
---
---comm a b = a * b - b * a
---ehh = elements :: [Elem (Tensor H H)]
---
---sym = [ x `tensor` y + y `tensor` x 
---      | x <- elements, y <- elements, x <= y ] 
---        :: [Elem (Tensor H H)]
---
---ske = [ x `tensor` y - y `tensor` x 
---      | x <- elements, y <- elements, x < y ] 
---        :: [Elem (Tensor H H)]
---
---showProd a b = 
---    [ (printf "[%18s ,%18s ] = " (show x) (show y))  ++ show (comm x y) 
---        | x <- a, y <- b, x < y]
---
---
----- Create a new more convenient basis for H Tensor H
----- Need to give names, and elements
----- Then, expand arbitary elements in the new basis
---data TauBasis = Sym H H | Skew H H
----- instance Basis TauBasis where
-----     type VS TauBasis = Tensor H H
---basisT = [ Sym  a a | a <- [E,I,J,K]]
---      ++ [ Sym  E a | a <- [I,J,K]]
---      ++ [ Sym  J K , Sym  K I, Sym  I J]
---      ++ [ Skew E a | a <- [I,J,K]]
---      ++ [ Skew J K , Skew K I, Skew I J]
---embedT (Sym  a b) = (embed a) `tensor` (embed b) + (embed b) `tensor` (embed a)
---embedT (Skew a b) = (embed a) `tensor` (embed b) - (embed b) `tensor` (embed a)
---
---instance Show TauBasis where
---    show (Sym  a b) = show a ++ " \x2228 " ++ show b
---    show (Skew a b) = show a ++ " \x2227 " ++ show b
+instance Algebra (V H) where
+    unit    = e
+    mul x y = mu (x `tensor` y)
 
+-- Now lets make Tensor H H an algebra
+instance Algebra (V (Tensor H H)) where
+    unit = e `tensor` e
+    mul x y = extend muHH (x `tensor` y)
+            where 
+                muHH  (Tensor (Tensor x y) (Tensor x' y')) 
+                    = ((return x) * (return x')) `tensor` ((return y') * (return y))
+
+comm a b = a * b - b * a
+ehh = map return elements :: [V (Tensor H H)]
+
+sym0 = [ x `tensor` x  | x <- [e,i,j,k]]
+sym1 = [ (e `tensor` x + x `tensor` e) | x <- [i,j,k]]
+sym2 = [ j `tensor` k + k `tensor` j, k `tensor` i + i `tensor` k, i `tensor` j + j `tensor` i]
+sym = sym0 ++ sym1 ++ sym2
+
+ske1 = [ (e `tensor` x - x `tensor` e) | x <- [i,j,k]]
+ske2 = [ j `tensor` k - k `tensor` j, k `tensor` i - i `tensor` k, i `tensor` j - j `tensor` i]
+ske = ske1 ++ ske2
+
+
+showProd a b = 
+    [ (printf "[%18s ,%18s ] = " (show x) (show y))  ++ show (comm x y) 
+        | x <- a, y <- b, x < y]
+
+showProdInBasis bs xs ys = 
+    let sh = showInBasis bs
+    in  [ (printf "[%10s ,%10s ] = " (sh x) (sh y))  ++ sh (comm x y) 
+            | x <- xs, y <- ys]
+
+
+-- Create a new more convenient basis for H Tensor H
+-- Need to give names, and elements
+-- Then, expand arbitary elements in the new basis
+data TauBasis = Sym H H | Skew H H deriving (Ord, Eq)
+instance Show TauBasis where
+    show (Sym  a b) = show a ++ " \x2228 " ++ show b
+    show (Skew a b) = show a ++ " \x2227 " ++ show b
+
+instance FiniteSet TauBasis where
+    elements =  [ Sym  a a | a <- [E,I,J,K]]
+             ++ [ Sym  E a | a <- [I,J,K]]
+             ++ [ Sym  J K , Sym  K I, Sym  I J]
+             ++ [ Skew E a | a <- [I,J,K]]
+             ++ [ Skew J K , Skew K I, Skew I J]
+
+instance Basis TauBasis (Tensor H H) where
+    --eta   :: TauBasis -> (Tensor H H) -> R
+    eta (Sym  a b) (Tensor x y) = 0.5 * (delta a x * delta b y + delta b x * delta a y)
+    eta (Skew a b) (Tensor x y) = 0.5 * (delta a x * delta b y - delta b x * delta a y)
+
+tau = elements :: [TauBasis]
 
 
 
